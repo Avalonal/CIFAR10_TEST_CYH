33a03d05c5e88769ca72404378304401a7a5dedb
Avalonal <1433452606@qq.com>
2021-10-07 18:38:27 +0800

    调参训练

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ train_transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
 
     transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), ])
 
 test_transform = transforms.Compose(
     [transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), ])
 
 classes = ('plane',
            'car',
@@ -30,7 +30,7 @@ classes = ('plane',
 dataDir = './data/'
 modelDir = '.\\model'
 
-stepSize = 1000
+stepSize = 10
 
 epoches = 50
 
--- datasets.py
+++ datasets.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 
 
 trainset = CIFAR10(root=dataDir, train=True, download=False, transform=train_transform)
-trainloader = DataLoader(trainset, batch_size=16, shuffle=True, num_workers=2)
+trainloader = DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
 testset = CIFAR10(root=dataDir, train=False, download=False, transform=test_transform)
-testloader = DataLoader(testset, batch_size=16, shuffle=False, num_workers=2)
+testloader = DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
